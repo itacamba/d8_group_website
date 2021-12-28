07cc3746a19b902ac338b7554e55de70eddc9aed
itacamba <daniela.sahagun03@gmail.com>
2021-12-28 14:06:52 -0500

    finished 'main' section, working responsive, added chessboard.'

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,18 +8,16 @@ const Home = () => {
             <main>
                 <div className="main-content">
                     <h2>The New Way to Win</h2>
-                    <h3>Win Federal Government Contracts</h3>
+                    <h3>Federal Government Contracts</h3>
                     <p>d8 Group helps you assess the entire federal, 
-                    state and local market space to determine where 
-                    to make your next move. Learn who is buying exactly 
-                    what you are selling within a specified period of time.</p>
+                    state and local market space to determine
+                    who is buying exactly what you are selling.</p>
                 </div>
-                <div className="gradient-right-light"></div>
-                <div className="gradient-left-light"></div>
+                <div className="gradient-bottom"></div>
+                <div className="chessboard-img">
+                        <img src={Chessboard} alt="chessboard and queen" />
+                </div>
             </main>
-            <div className="chessboard-img">
-                <img src={Chessboard} alt="chessboard and queen" />
-            </div>
             <div className="test"></div>
         </div>
     );
