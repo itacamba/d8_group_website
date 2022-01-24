f303277dbd7c846da7d6815a35a9044c89653bf7
itacamba <daniela.sahagun03@gmail.com>
2022-01-24 18:16:41 -0500

    changed pageheading background img

--- src/Components/Home/Home-Components/CallToAction/CallToAction.js
+++ src/Components/Home/Home-Components/CallToAction/CallToAction.js
@@ -4,7 +4,7 @@ import './CallToAction.css'
 const CallToAction = () => {
     return (
         <div className="call-to-action-banner">
-            <h2>Learn about how our methodology <br></br>
+            <h2>Learn about our methodology <br></br>
             is helping our clients grow today!</h2>
             <p>d8Group increases your Probability of Win (pWin)</p>
             <a href="/contact" className="main-button">Contact Us</a>
