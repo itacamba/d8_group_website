fd7ddc60955bf9e07b2d8d6157b674790f24d488
cyCarla <carla@cydeploy.com>
2022-03-07 13:40:52 -0500

    Added Carla and Robin

--- src/Components/Pages/Leadership/Leadership.js
+++ src/Components/Pages/Leadership/Leadership.js
@@ -21,6 +21,8 @@ import Tom from '../../../Images/tom_giroir.jpeg';
 import Jason from '../../../Images/jason_cullum.jpeg';
 import Les from '../../../Images/les_mcfarling.jpeg';
 import Ken from '../../../Images/ken_algood.jpeg';
+import Robin from '../../Images/robin_kelly.jpg';
+import Carla from '../../Images/carla_sah.jpg';
 
 
 
@@ -29,118 +31,124 @@ const Leadership = () => {
 
     const leaders = [
         {
-            id: 1,
             name: 'Reeba Magulick',
             title: 'Founding Partner',
-            url: 'https://www.linkedin.com/in/reeba-magulick/'
+            url: 'https://www.linkedin.com/in/reeba-magulick/',
+            img: Reeba,
         },
         {  
-            id: 2,
             name: 'William Kerr',
             title: 'Senior Capture Strategist',
-            url: 'https://www.linkedin.com/in/william-kerr-4a975258/'
+            url: 'https://www.linkedin.com/in/william-kerr-4a975258/',
+            img: Bill,
         },
         {
-            id: 3,
             name: 'Hayden Pond',
             title: 'Director of Strategic Growth',
-            url: 'https://www.linkedin.com/in/hayden-pond-2b7aa199/'
+            url: 'https://www.linkedin.com/in/hayden-pond-2b7aa199/',
+            img: Hayden
         },
         {
-            id: 4,
             name: 'Adam Ball',
             title: 'Director of Strategic Growth',
-            url: 'https://www.linkedin.com/in/adam-ball-12062952/'
+            url: 'https://www.linkedin.com/in/adam-ball-12062952/',
+            img: Adam,
         },
         {
-            id: 5,
             name: 'TJ Magulick',
             title: 'Principal of Business Operations',
-            url: 'https://www.linkedin.com/in/tj-magulick-7379521/'
+            url: 'https://www.linkedin.com/in/tj-magulick-7379521/',
+            img: Tj
         },
         {
-            id: 6,
             name: 'Sarah Malone',
             title: 'Business Intelligence/ Capture Analyst',
-            url: 'https://www.linkedin.com/in/sarah-malone-244406211/'
+            url: 'https://www.linkedin.com/in/sarah-malone-244406211/',
+            img: Sarah
         },
-        // {
-        //     id: 7,
-        //     name: 'Carla Sahagun',
-        //     title: 'Business Intelligence/ Capture Analyst',
-        //     url: '/'
-        // },
         {
-            id: 8,
+            name: 'Carla Sahagun',
+            title: 'Business Intelligence/ Capture Analyst',
+            url: 'https://www.linkedin.com/in/carla-sahagun/',
+            img: Carla
+        },
+        {
+            name: 'Robin Kelly',
+            title: 'Digital Marketing Strategist',
+            url: 'https://www.linkedin.com/in/robin-kelly/',
+            img: Robin
+        },
+        {
             name: 'Dr. Ellen Embrey',
             title: 'Advisor: Health Affairs',
-            url: 'https://www.linkedin.com/in/ellen-embrey-06389920/'
+            url: 'https://www.linkedin.com/in/ellen-embrey-06389920/',
+            img: Ellen
         },
         {
-            id: 9,
             name: 'Dr. Keith Salzman',
             title: 'Chief Medical Information Officer',
-            url: 'https://www.linkedin.com/in/klsalzman/'
+            url: 'https://www.linkedin.com/in/klsalzman/',
+            img: Keith,
         },
         {
-            id: 10,
             name: 'Rohan Patel',
             title: 'Lead Associate',
-            url: 'https://www.linkedin.com/in/rohan-patel-606430111/'
+            url: 'https://www.linkedin.com/in/rohan-patel-606430111/',
+            img: Rohan
         },
         {
-            id: 11,
             name: 'Eric Gregory',
             title: 'Advisor: Proposal & Capture',
-            url: 'https://www.linkedin.com/in/egregory1/'
+            url: 'https://www.linkedin.com/in/egregory1/',
+            img: Eric
         },
         {
-            id: 12,
             name: 'Amy Strouse',
             title: 'Advisor: Strategy',
-            url: 'https://www.linkedin.com/in/amy-strouse-1938b54/'
+            url: 'https://www.linkedin.com/in/amy-strouse-1938b54/',
+            img: Amy
         },
         {
-            id: 13,
             name: 'Seth Robbins, Esq',
             title: 'Subject Matter Expert',
-            url: 'https://www.linkedin.com/in/sethrobbinsattorney/'
+            url: 'https://www.linkedin.com/in/sethrobbinsattorney/',
+            img: Seth
         },
         {
-            id: 14,
             name: 'Joel Henson',
             title: 'Advisor: Big Win Capture',
-            url: 'https://www.linkedin.com/in/joelhenson/'
+            url: 'https://www.linkedin.com/in/joelhenson/',
+            img: Joel
         },
         {
-            id: 15,
             name: 'Toby Gouker, PhD',
             title: 'Advisor: Cyber Security',
-            url: 'https://www.linkedin.com/in/toby-gouker-phd-chisl-gslc-cism-cpem-5285901/'
+            url: 'https://www.linkedin.com/in/toby-gouker-phd-chisl-gslc-cism-cpem-5285901/',
+            img: Toby
         },
         {
-            id: 16,
             name: 'Tom Giroir',
             title: 'Subject Matter Expert',
-            url: 'https://www.linkedin.com/in/tomgiroir/'
+            url: 'https://www.linkedin.com/in/tomgiroir/',
+            img: Tom
         },
         {
-            id: 17,
             name: 'Jason Cullum',
             title: 'Advisor: Healthcare IT',
-            url: 'https://www.linkedin.com/in/jasoncullum/'
+            url: 'https://www.linkedin.com/in/jasoncullum/',
+            img: Jason
         },
         {
-            id: 18,
             name: 'Dr. Les McFarling',
             title: 'Mental Health Expert',
-            url: 'https://www.linkedin.com/in/les-mcfarling-84a598140/'
+            url: 'https://www.linkedin.com/in/les-mcfarling-84a598140/',
+            img: Les
         },
         {
-            id: 19,
             name: 'Ken Allgood',
             title: 'Federal Health Subject Matter Expert',
-            url: 'https://www.linkedin.com/in/kenallgood/'
+            url: 'https://www.linkedin.com/in/kenallgood/',
+            img: Ken
         }
     ]
     return (
@@ -152,45 +160,7 @@ const Leadership = () => {
                     leaders.map((leader, i) => 
                     <div className="gallery-card" key={i}>
                         <div className="gallery-card-img">
-                            {
-                                leader.id === 1? 
-                                <img src={Reeba} alt="Reeba Magulick" />
-                                : leader.id === 2?
-                                <img src={Bill} alt="William Kerr" />
-                                : leader.id === 3? 
-                                <img src={Hayden} alt="Hayden Pond" />
-                                : leader.id === 4?
-                                <img src={Adam} alt="Adam Ball" />
-                                : leader.id === 5?
-                                <img src={Tj} alt="TJ Magulick" />
-                                : leader.id === 6?
-                                <img src={Sarah} alt="Sarah Malone" />
-                                : leader.id === 8?
-                                <img src={Ellen} alt="Dr. Ellen Embrey" />
-                                : leader.id === 9?
-                                <img src={Keith} alt="Dr. Keith Salzman" />
-                                : leader.id === 10?
-                                <img src={Rohan} alt="Rohan Patel" />
-                                : leader.id === 11?
-                                <img src={Eric} alt="Eric Gregory" />
-                                : leader.id === 12?
-                                <img src={Amy} alt="Amy Strouse" />
-                                : leader.id === 13?
-                                <img src={Seth} alt="Seth Robbins, Esq" />
-                                : leader.id === 14?
-                                <img src={Joel} alt="Joel Henson" />
-                                : leader.id === 15?
-                                <img src={Toby} alt="Toby Gouker, PhD" />
-                                : leader.id === 16?
-                                <img src={Tom} alt="Tom Giroir" />
-                                : leader.id === 17?
-                                <img src={Jason} alt="Jason Cullum" />
-                                : leader.id === 18?
-                                <img src={Les} alt="Dr. Les McFarling" />
-                                : leader.id === 19?
-                                <img src={Ken} alt="Ken Allgood" />
-                                : null
-                            }
+                            <img src={leader.img} alt={leader.name} />
                         </div>
                         <h3>{leader.name}</h3>
                         <h3>{leader.title}</h3>
